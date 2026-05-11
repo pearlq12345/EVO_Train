77ac84f97965f23275381304de72f6743d5e9fd3
Ru-hulu <2422780148@qq.com>
2026-05-11 18:30:37 +0800

    update nas mount

--- train/send_pai_request.py
+++ train/send_pai_request.py
@@ -27,7 +27,7 @@ DEFAULT_ROUTE = "eth1"
 DEFAULT_DATA_SOURCES = [
     CreateJobRequestDataSources(data_source_id="d-xfobl8zdj3cqdrleqo", mount_path="/mnt/pai/data/"), # 数据集的id，以及希望数据集挂载在拉起的容器的什么路径
     CreateJobRequestDataSources(data_source_id="d-hzpwiw5qvtyy7887oe", mount_path="/mnt/code/"), # 由于容器无法使用clone，所以暂时将代码也用数据集的方式进行管理
-    CreateJobRequestDataSources(data_source_id="d-cch5i1noybcyyxtyr2", mount_path="/mnt/usrresult/"), 
+    # CreateJobRequestDataSources(data_source_id="d-cch5i1noybcyyxtyr2", mount_path="/mnt/usrresult/"), 
     # CreateJobRequestDataSources(uri="nas://001vtgf4opoobb8u5gh.cn-hangzhou/", mount_path="/mnt/usrresult", mount_access="RW")
 ]
 
